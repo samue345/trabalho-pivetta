3dc17fff46ba1508a872a622d722d06757fbec71
Samuel Souza <85644942+samue345@users.noreply.github.com>
2022-11-14 15:10:06 -0300

    fim

---
+++ inserifim.h
@@ -0,0 +1,121 @@
+#ifndef iNSERIFIM_H
+#define INSERIFIM_H
+#include "estruturas.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+Lista *ultimo(Lista *lista)
+{
+   Lista* aux=lista;
+   while (aux->prox != NULL)
+      aux=aux->prox;
+   return aux;
+   
+}
+ListaP *ultimoP(ListaP *lista)
+{
+   ListaP* aux=lista;
+   while (aux->prox != NULL)
+      aux=aux->prox;
+   return aux;
+   
+}
+ListaC *ultimoC(ListaC *lista)
+{
+   ListaC* aux=lista;
+   while (aux->prox != NULL)
+      aux=aux->prox;
+   return aux;
+   
+}
+ListaT *ultimoT(ListaT *lista)
+{
+   ListaT* aux=lista;
+   while (aux->prox != NULL)
+      aux=aux->prox;
+   return aux;
+   
+}
+
+Lista *inserirFim(Lista *lista, Aluno *aluno)
+{
+   if(lista != NULL)
+   {
+      Lista *temp = lista;
+      while (temp != NULL)
+      {
+         if(lista->info == aluno)
+            return lista;
+
+         temp = temp->prox;
+      }
+   }
+   Lista *novo=(Lista*)malloc(sizeof(Lista));
+   if(lista != NULL)
+      ultimo(lista)->prox=inserirInicio(NULL, aluno);
+      return lista == NULL ? inserirInicio(NULL, aluno) : lista;
+   
+
+}
+ListaP *inserirFimP(ListaP *lista, Professor *professor)
+{
+   if(lista != NULL)
+   {
+      ListaP *temp = lista;
+      while (temp != NULL)
+      {
+         if(lista->info == professor)
+            return lista;
+
+         temp = temp->prox;
+      }
+   }
+   ListaP *novo=(ListaP*)malloc(sizeof(ListaP));
+   if(lista != NULL)
+      ultimoP(lista)->prox=inserirInicioP(NULL, professor);
+      return lista == NULL ? inserirInicioP(NULL, professor) : lista;
+   
+
+}
+ListaC *inserirFimC(ListaC *lista, Cadeira *cadeira)
+{
+   if(lista != NULL)
+   {
+      ListaC *temp = lista;
+      while (temp != NULL)
+      {
+         if(lista->info == cadeira)
+            return lista;
+
+         temp = temp->prox;
+      }
+   }
+   ListaC *novo=(ListaC*)malloc(sizeof(ListaC));
+   if(lista != NULL)
+      ultimoC(lista)->prox=inserirInicioC(NULL, cadeira);
+      return lista == NULL ? inserirInicioC(NULL, cadeira) : lista;
+   
+
+}
+ListaT *inserirFimT(ListaT *lista, Turma *turma)
+{
+   if(lista != NULL)
+   {
+      ListaT *temp = lista;
+      while (temp != NULL)
+      {
+         if(lista->info == turma)
+            return lista;
+
+         temp = temp->prox;
+      }
+   }
+   ListaT *novo=(ListaT*)malloc(sizeof(ListaT));
+   if(lista != NULL)
+      ultimoT(lista)->prox=inserirInicioT(NULL, turma);
+      return lista == NULL ? inserirInicioT(NULL, turma) : lista;
+   
+
+}
+
+#endif
